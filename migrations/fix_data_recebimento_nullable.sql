c2b109aa99c98140f71227c52d655c23d764f405
Julio Lopes <juliolopes.dev@gmail.com>
2026-01-12 09:19:40 -0300

    fix: adicionar limpeza de dataRecebimento para NFs antigas não conferidas

--- migrations/fix_data_recebimento_nullable.sql
+++ migrations/fix_data_recebimento_nullable.sql
@@ -7,5 +7,16 @@ ALTER TABLE notas_fiscais
 ALTER COLUMN data_recebimento DROP DEFAULT,
 ALTER COLUMN data_recebimento DROP NOT NULL;
 
+-- Limpar dataRecebimento de NFs que ainda não foram conferidas
+-- (NFs com status que indica que a mercadoria ainda não foi fisicamente recebida)
+UPDATE notas_fiscais 
+SET data_recebimento = NULL 
+WHERE status IN (
+  'AGUARDANDO_CONFERENCIA', 
+  'PENDENTE_TRANSFERENCIA', 
+  'EM_TRANSITO',
+  'AGUARDANDO_CONFERENCIA_DESTINO'
+);
+
 -- Comentário explicativo
 COMMENT ON COLUMN notas_fiscais.data_recebimento IS 'Data em que a mercadoria foi fisicamente recebida (preenchido na conferência de volumes)';
